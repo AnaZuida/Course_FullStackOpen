dcaa7840f4ec31ea882364ea702709b5062c2dc0
AnaZuida <anatsuida@gmail.com>
2023-09-04 14:11:49 +0300

    2e maiden tiedot started

---
+++ osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,147 @@
+import { useState, useEffect } from 'react'
+import countriesService from './services/countries'
+import weatherService from './services/weather'
+
+const Notification =  ({ message }) => {
+  if(message === null) {
+    return null
+  }
+
+  return (
+    <div className="notification">
+      {message}
+    </div>
+  )  
+}
+
+const SearchForm = ({newFilter, handleFilterChange}) => {
+  return (
+    <form>
+      <div>
+        Find countries <input value={newFilter} onChange={handleFilterChange} />  
+      </div>
+    </form>
+  )
+}
+
+const ListCountries = ({countryNames, newFilter, errorMessage}) => {
+  let countriesToShow = newFilter ? countryNames.filter(countryName => countryName.includes(newFilter)) : countryNames
+  console.log(newFilter)
+  //console.log(countryNames)
+  //console.log(countriesToShow)
+
+  if(countriesToShow === null) {
+    return null
+  } else if (countriesToShow.length > 10) {
+    return errorMessage.setErrorMessage(`Too many matches. Please specify another filter.`)
+  } else if (countriesToShow.length == 1) {
+    return <CountryInfo countryToShow={countriesToShow[0]} />
+  }
+
+  return (
+    <div>
+      <ul>
+        {countriesToShow.map(country => (
+          <li>{country}<button onClick={() => CountryInfo(country)}>show</button></li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const CountryInfo = ({countryToShow}) => {
+  let countryCapital = null
+  countriesService
+    .getCountry(countryToShow)
+      .then(country => {
+        countryCapital = country.capital
+        return (
+          <div>
+            <div><h2>{country.name.common}</h2></div>
+            <div>
+              Capital: {country.capital}<br />
+              Area: {country.area}
+            </div>
+            <div>
+              <b>Languages:</b><br />
+              <ul>
+              {country.languages.map(language => (
+                <li>{language}</li>
+              ))}
+              </ul>
+            </div>
+            <div>
+              <img src={country.flags.png} alt={country.flags.alt} />
+            </div>
+          </div>
+        )
+      })
+      /* .catch(error) {
+            setErrorMessage(`Country was not found from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })
+      */
+      
+  weatherService
+    .getWeather(countryCapital)
+      .then(weather => {
+        return (
+            <div>
+              <b>Weather in {countryCapital}</b>
+              Temperature: {weather.temp_c} Celcius
+              <img src='http:{weather.condition.icon}'>{weather.condition.text}</img>
+              Wind: {weather.wind_kph} Km/h
+            </div>
+        )
+      })
+  /*
+  return (
+    <div>
+      
+    </div>
+  )
+  */
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [countryNames, setCountryNames] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  //let countryNames = []
+  //let countriesToShow = null
+  //let countriesToShow = newFilter ? countryNames.filter(countryName => countryName.includes(newFilter)) : countryNames
+
+  useEffect(() => {
+    countriesService
+      .getAll()
+        .then(initialCountries => {
+          setCountries(initialCountries)
+          //setCountryNames(initialCountries.name)
+           //countryNames = countries.map(({name}) => name.common)
+           setCountryNames(countries.map(({name}) => name.common))
+           // console.log(countryNames)
+        })   
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    //countriesToShow = newFilter ? countryNames.filter(countryName => countryName.includes(newFilter)) : countryNames
+    //console.log(countriesToShow)
+  }
+
+  return (
+    <div>
+      <h2>Country Info Service</h2>
+      <SearchForm newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <ListCountries countryNames={countryNames} newFilter={newFilter} errorMessage={errorMessage} />
+      <Notification message={errorMessage} />
+    </div>
+  )
+
+}
+
+export default App;
--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import personsService from './services/persons'
+
+const Notification =  ({ message, error }) => {
+  if(message === null) {
+    return null
+  }
+
+  if(error === true) {
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  } else {
+    return (
+      <div className="notification">
+        {message}
+      </div>
+    )  
+  }
+}
 
 const FilterForm = ({newFilter, handleFilterChange}) => {
   // Component for filtering phonebook
@@ -25,14 +47,14 @@ const InputForm = ({addName, newName, newNumber ,handleNameChange, handleNumberC
   )
 }
 
-const ListNames = ({persons, newFilter}) => {
+const ListNames = ({persons, newFilter, deleteContact}) => {
   const namesToShow = newFilter ? persons.filter(person => person.name.includes(newFilter)) : persons
   return (
     <div>
       <table><tbody>
         <tr><td><b>Name</b></td><td><b>Number</b></td></tr>
         {namesToShow.map(person => (
-          <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>
+          <tr key={person.name}><td>{person.name}</td><td>{person.number}</td><td><button onClick={() => deleteContact(persons, person.id, namesToShow)}>Delete</button></td></tr>
         ))}  
       </tbody></table>
     </div>
@@ -40,18 +62,21 @@ const ListNames = ({persons, newFilter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-  
-  
-  
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [notificationType, setNotificationType] = useState(false)
+
+  useEffect(() => {
+    personsService
+      .getAll()
+        .then(initialPersons => {
+          setPersons(initialPersons)
+        })   
+  }, [])
+
   const addName = (event) => {
     event.preventDefault()
     //console.log('button clicked', event.target)
@@ -62,12 +87,69 @@ const App = () => {
       number: newNumber
     }
     //console.log(nameObject)
+
     if (!persons.some(person => person.name === nameObject.name)) {
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
+      //setPersons(persons.concat(nameObject))
+      personsService
+        .create(nameObject)
+          .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            setErrorMessage(`Added ${returnedPerson.name}`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })
     } else {
-      alert(`Name ${newName} already exists in the phonebook`)
+      //alert(`Name ${newName} already exists in the phonebook`)
+      if (window.confirm(`Name ${nameObject.name} is already in phonebook. Do you want to replace old number with new one?`)) {
+        const personToBeUpdated = persons.find(person => person.name === nameObject.name) 
+        const updatedEntry = {...personToBeUpdated, number: newNumber}
+        personsService
+          .update(personToBeUpdated.id, updatedEntry)
+            .then(updatedPerson => {
+              setPersons(persons.map(person => person.id !== personToBeUpdated.id ? person : updatedPerson))
+              setNewName('')
+              setNewNumber('')
+              setErrorMessage(`Modified ${personToBeUpdated.name}`)
+              setTimeout(() => {
+                setErrorMessage(null)
+              }, 5000)
+            })
+          .catch(error => {
+            setNotificationType(true)
+            setErrorMessage(`Person ${nameObject.name} was already removed from the server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+              setNotificationType(false)
+            }, 5000)
+          })
+      }
+    }
+  }
+
+  const deleteContact = (persons, id) => {
+    //console.log('delete contact')
+    const personToBeRemoved = persons.find(person => person.id === id) 
+    if (window.confirm(`Are you sure you want to remove ${personToBeRemoved.name} from phonebook?`)) {
+      personsService
+        .remove(id)
+          .then(removedId => {
+            setPersons(persons.filter(person => person.id !== id))
+            setErrorMessage(`Removed ${personToBeRemoved.name} from phonebook`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setNotificationType(true)
+            setErrorMessage(`Person ${personToBeRemoved.name} you tried to remove did not exist`)
+            setTimeout(() => {
+              setErrorMessage(null)
+              setNotificationType(false)
+            }, 5000)
+          })
     }
   }
 
@@ -87,11 +169,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} error={notificationType} />
       <FilterForm newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h2>Add new phonebook entry</h2>
       <InputForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <ListNames persons={persons} newFilter={newFilter}/>
+      <ListNames persons={persons} newFilter={newFilter} deleteContact={deleteContact}/>
     </div>
   )
 
